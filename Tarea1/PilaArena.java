20d98bf2dc2daef0cf2fd7d17bf899d1224b7cab
Cristóbal Mesías <32551243+cmesiasd@users.noreply.github.com>
2018-09-24 21:44:17 -0300

    Subiendo archivos .java

---
+++ Tarea1/PilaArena.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class PilaArena {
+
+  public puntoCentral(int lado){
+    int x;
+    int y;
+    if(lado%2 == 0){
+      x = lado/2 + 1;
+      y = lado/2 + 1;
+    }
+    else{
+      x = lado/2;
+      y = lado/2;
+    }
+  }
+
+  public crearMatriz(){
+      int matriz[][], nLado;
+      Ventana window = new Ventana(n, "proban2");
+      window.mostrarMatriz(mat);
+
+    }
+
+  }
+
+  public recorrerMatriz(){
+
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("N?");
+    int n = sc.nextInt();
+
+  }
+}
